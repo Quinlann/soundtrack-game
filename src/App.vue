368ed79b65be80b92d263f34168411f36dacbace
Quinlann <hegs_hta@hotmail.com>
2023-03-30 13:47:10 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<template>
+  <h1>Hello World of soundtracks!</h1>
+</template>
+
+<style scoped land="less">
+</style>
